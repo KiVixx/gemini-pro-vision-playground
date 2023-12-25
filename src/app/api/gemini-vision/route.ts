512490e7a2833c282abf76c2523c8e0fe23646f8
KeyVix <king.ho728@gmail.com>
2023-12-25 14:01:26 +0800

    Update route.ts

--- src/app/api/gemini-vision/route.ts
+++ src/app/api/gemini-vision/route.ts
@@ -1,11 +1,16 @@
 // api/gemini-vision/route.ts
 import { GeneralSettings } from "@/types";
-import {
+// import {
+//   HarmCategory,
+//   HarmBlockThreshold,
+//   GenerateContentRequest,
+// } from "@google/generative-ai";
+import { 
+  GoogleGenerativeAI
   HarmCategory,
   HarmBlockThreshold,
   GenerateContentRequest,
-} from "@google/generative-ai";
-import { GoogleGenerativeAI } from '@fuyun/generative-ai'
+} from '@fuyun/generative-ai'
 
 const mapSafetyValueToThreshold = (value: number): HarmBlockThreshold => {
   switch (value) {
